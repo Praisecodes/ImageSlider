d8e9d8f028fa2960d295a5081a3182eb2b8a202b
Praisecodes <cmancoder@gmail.com>
2022-02-15 12:36:52 +0100

    Update JavaScript

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,28 @@
 // Code By A JavaScript Beginner (Praise Codes)
 
 var images = [
-    "http://127.0.0.1:5500/Images/1.jpg",
-    "http://127.0.0.1:5500/Images/2.jpg",
-    "http://127.0.0.1:5500/Images/3.jpg",
-    "http://127.0.0.1:5500/Images/4.jpeg"
+    "./Images/1.jpg",
+    "./Images/2.jpg",
+    "./Images/3.jpg",
+    "./Images/4.jpeg"
 ];
 
 var chk = 0;
 
 const Next = () =>{
     var currentImage = document.getElementById("ImageDisplayed");
-    images.forEach(im=>{
-        if(currentImage.src == im){
-            chk = images.indexOf(im);
-            chk++
-        }
-    });
-    if(chk == 4){
-        currentImage.src = images[0];
-    }else{
-        currentImage.src = images[chk];
+    chk++
+    if(chk>=images.length){
+        chk = 0;
     }
+    currentImage.src = images[chk];
 }
 
 const Prev = () => {
     var currentImage = document.getElementById("ImageDisplayed");
-    images.forEach(im=>{
-        if(currentImage.src == im){
-            chk = images.indexOf(im);
-            chk--;
-        }
-    });
-
-    if(chk == -1){
-        currentImage.src = images[3];
-        chk = 0;
-    }else{
-    currentImage.src = images[chk];}
+    chk--;
+    if(chk < 0){
+        chk = images.length-1;
+    }
+    currentImage.src = images[chk];
 }
